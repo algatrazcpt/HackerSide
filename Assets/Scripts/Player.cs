2ea6baf4dcef5a17d8ad84afd43c66c3bc123162
Atakann <53114980+algatrazcpt@users.noreply.github.com>
2024-11-23 21:29:13 +0300

    up

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,12 +7,25 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    }
+    public float speed = 5f; // Karakterin yürüme hýzý
+
+    private void Update()
     {
-        
+        // Karakterin hareketini kontrol etme
+        float horizontal = Input.GetAxis("Horizontal")*-1;
+        float vertical = Input.GetAxis("Vertical")*-1;
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            transform.Translate(direction * speed * Time.deltaTime, Space.World);
+
+            // Yönlendirme
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
+        }
     }
 }
